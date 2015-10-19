547e66ad56d2046f7964ce9968b4b2b158361ecb
Christian Blank <christian.blank@haw-hamburg.de>
2015-10-19 11:44:05 +0200

    Rename interface and start with implementation

--- CAD2Unity/3DXMLLoader/Implementation/Model/IThreeDArchive.cs
+++ CAD2Unity/3DXMLLoader/Implementation/Model/IThreeDArchive.cs
@@ -1,12 +1,25 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Xml.Linq;
 
 namespace ThreeDXMLLoader.Implementation.Model
 {
-
-    interface IThreeDArchiv
+    /// <summary>
+    /// Interface for working with 3DXML archive.
+    /// </summary>
+    interface IThreeDArchive
     {
+        /// <summary>
+        /// Returns the manifest file of the archive.
+        /// </summary>
+        /// <returns>the manifest as XDocument</returns>
         XDocument GetManifest();
 
+        /// <summary>
+        /// Returns a document with the specified name from the archive.
+        /// </summary>
+        /// <param name="name">The name of the requested document</param>
+        /// <returns>the requested document as XDocument</returns>
+        /// <exception cref="Exception">Thrown when file with <paramref name="name"/> was not found in archive.</exception>
         XDocument GetNextDocument(string name);
     }
 }
--- CAD2Unity/3DXMLLoader/Implementation/Model/ThreeDRepFile.cs
+++ CAD2Unity/3DXMLLoader/Implementation/Model/ThreeDRepFile.cs
@@ -1,9 +1,38 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Xml.Linq;
+using Ionic.Zip;
 
 namespace ThreeDXMLLoader.Implementation.Model
 {
-    class ThreeDRepFile : IThreeDArchiv
+    /// <summary>
+    /// In memory archive implementation for the IThreeDArchive interface.
+    /// </summary>
+    class ThreeDRepFile : IThreeDArchive
     {
+        private IDictionary<string, Stream> _files; 
+
+        private ThreeDRepFile()
+        {
+            
+        }
+
+        public static IThreeDArchive Create(Stream data)
+        {
+            var archive = new ThreeDRepFile();
+            using (var zipArchive = ZipFile.Read(data))
+            {
+                foreach (var entry in zipArchive)
+                {
+                    
+                }
+            }
+
+            return archive;
+        }
+
         public XDocument GetManifest()
         {
             throw new System.NotImplementedException();
