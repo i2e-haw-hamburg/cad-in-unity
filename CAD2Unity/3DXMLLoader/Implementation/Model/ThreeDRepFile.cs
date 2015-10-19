ba767df5dd51b50e3d41df8dcd5f77c345df7514
Christian Blank <christian.blank@haw-hamburg.de>
2015-10-19 12:12:45 +0200

    Implement the in memory archive

--- CAD2Unity/3DXMLLoader/Implementation/Model/ThreeDRepFile.cs
+++ CAD2Unity/3DXMLLoader/Implementation/Model/ThreeDRepFile.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
+using System.Xml;
 using System.Xml.Linq;
 using Ionic.Zip;
 
@@ -12,35 +14,53 @@ namespace ThreeDXMLLoader.Implementation.Model
     /// </summary>
     class ThreeDRepFile : IThreeDArchive
     {
-        private IDictionary<string, Stream> _files; 
+        private IDictionary<string, Stream> _files;
+        private const string ManifestName = "Manifest.xml";
 
-        private ThreeDRepFile()
+        private ThreeDRepFile(IDictionary<string, Stream> files)
         {
-            
+            _files = files;
         }
 
+        /// <summary>
+        /// Creates a new in memory archive from a given stream of zipped data.
+        /// </summary>
+        /// <param name="data">the zip compressed 3dxml archive</param>
+        /// <returns>a new instance of a ThreeDRepFile</returns>
         public static IThreeDArchive Create(Stream data)
         {
-            var archive = new ThreeDRepFile();
+            var dict = new Dictionary<string, Stream>();
             using (var zipArchive = ZipFile.Read(data))
             {
-                foreach (var entry in zipArchive)
+                foreach (var entry in zipArchive.Where(entry => !entry.IsDirectory))
                 {
-                    
+                    var name = entry.FileName.ToLower();
+                    var fileStream = new MemoryStream();
+                    entry.Extract(fileStream);
+                    dict.Add(name, fileStream);
                 }
             }
+            var archive = new ThreeDRepFile(dict);
 
             return archive;
         }
 
         public XDocument GetManifest()
         {
-            throw new System.NotImplementedException();
+            return GetNextDocument(ManifestName);
         }
 
         public XDocument GetNextDocument(string name)
         {
-            throw new System.NotImplementedException();
+            name = name.ToLower();
+            if (!_files.ContainsKey(name))
+            {
+                throw new Exception($"File {name} not found in archive.");
+            }
+            var fileStream = _files[name];
+            var reader = XmlReader.Create(fileStream);
+            reader.MoveToContent();
+            return XDocument.Load(reader);
         }
     }
 
